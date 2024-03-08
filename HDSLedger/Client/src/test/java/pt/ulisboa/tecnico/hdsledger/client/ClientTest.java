d890fa68a53dfa41c91baff5084ba96471773808
nemisis84 <simen.myrrusten@gmail.com>
2024-03-08 13:37:42 +0000

    README and removed unused imports

--- HDSLedger/Client/src/test/java/pt/ulisboa/tecnico/hdsledger/client/ClientTest.java
+++ HDSLedger/Client/src/test/java/pt/ulisboa/tecnico/hdsledger/client/ClientTest.java
@@ -1,9 +1,9 @@
-import pt.ulisboa.tecnico.hdsledger.client.Client;
+package pt.ulisboa.tecnico.hdsledger.client;
 
-import org.mockito.Mockito;
-import org.mockito.Mock;
-import org.mockito.InjectMocks;
-import org.junit.jupiter.api.Test;
+// import org.mockito.Mockito;
+// import org.mockito.Mock;
+// import org.mockito.InjectMocks;
+// import org.junit.jupiter.api.Test;
 
 public class ClientTest {
 
